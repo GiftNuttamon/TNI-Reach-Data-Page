8237828382e527d64b31dc4ac89372aef623f138
0764nuttamon <159672481+GiftNuttamon@users.noreply.github.com>
2025-08-27 08:49:31 +0700

    ADD Form

--- Node_Server/googleSheet.js
+++ Node_Server/googleSheet.js
@@ -3,9 +3,9 @@
 const { google } = require("googleapis");
 const path = require("path");
 
-const KEYFILEPATH = path.join(__dirname, "solar-idea-470104-r0-83b7f2684f00.json"); 
+const KEYFILEPATH = path.join(__dirname, "solar-idea-470104-r0-83b7f2684f00.json");  //API Keys Sheets (ใช้ ADD Credentials)
 const SCOPES = ["https://www.googleapis.com/auth/spreadsheets"];
-const SPREADSHEET_ID = "1G-htktut-n4Iy9hoZEWWKPsvlw44kDNlZw_Wb8YCDX0"; 
+const SPREADSHEET_ID = "1G-htktut-n4Iy9hoZEWWKPsvlw44kDNlZw_Wb8YCDX0"; //Sheets ID
 
 async function getAuthClient() {
   const auth = new google.auth.GoogleAuth({
@@ -26,7 +26,7 @@ async function addRow(sheetName, values) {
     range: `${sheetName}!A:A`, // อ่านจากคอลัมน์ A เท่านั้นเพื่อความเร็ว
   });
   const existingRows = response.data.values ? response.data.values.length : 0;
-  const newRowNumber = existingRows; // ลำดับใหม่ = จำนวนแถวเดิม + 1
+  const newRowNumber = existingRows; // ลำดับใหม่คนที่ ...
 
   // 2. เพิ่มลำดับคนที่ลงในอาร์เรย์ values
   const updatedValues = [...values, newRowNumber];
@@ -41,7 +41,7 @@ async function addRow(sheetName, values) {
       values: [updatedValues],
     },
   });
-  console.log("✅ Data added to Google Sheets");
+  console.log("✅ Data added to Google Sheets"); //แจ้งเตือนใน LOG ว่าต่อดาต้าเบสสมบูณร์
 }
 
 module.exports = { addRow };
--- Node_Server/server.js
+++ Node_Server/server.js
@@ -1,8 +1,10 @@
-const express = require("express");
+//วิธีเปิดดาต้าเบส open new terminal > cd Node_Server > node server.js จะมีแจ้งเตือน ✅ Data added to Google Sheets
+
+const express = require("express"); //ใช้ express
 const bodyParser = require("body-parser");
-const cors = require("cors");
+const cors = require("cors"); //Install Cors
 const path = require("path");
-const { addRow } = require("./googleSheet.js"); 
+const { addRow } = require("./googleSheet.js");  //เรียกใช้ Node googleSheets.js
 
 const app = express();
 app.use(bodyParser.json());
@@ -11,7 +13,7 @@ app.use(cors());
 // Route สำหรับรับข้อมูลจากฟอร์ม
 app.post("/submit", async (req, res) => {
   try {
-    const { fname, lname, grade, email, phone } = req.body;
+    const { fname, lname, grade, email, phone, lineid, country, fac, major } = req.body;
     
     const rowData = [
       fname, 
@@ -19,18 +21,22 @@ app.post("/submit", async (req, res) => {
       grade, 
       email, 
       phone, 
+      lineid,
+      country,
+      fac,
+      major,
       new Date().toLocaleString("en-US", { timeZone: "Asia/Bangkok" }), 
     ];
     
     // เรียกใช้ฟังก์ชัน addRow จากไฟล์ googleSheet.js
-    await addRow("INFO1", rowData);
-    res.send("✅ บันทึกข้อมูลเรียบร้อย");
+    await addRow("INFO1", rowData); //ชีตแผ่นชื่อ INFO1
+    res.send("✅ บันทึกข้อมูลเรียบร้อย"); //popup nofi
   } catch (err) {
     console.error(err);
-    res.status(500).send("❌ เกิดข้อผิดพลาด");
+    res.status(500).send("❌ เกิดข้อผิดพลาด"); //popup nofi unsuccess
   }
 });
 
 app.listen(3000, () => {
-  console.log("Server running at http://localhost:3000");
+  console.log("Server running at http://localhost:3000"); // port
 });
